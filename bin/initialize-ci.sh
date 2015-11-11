6df827752d5275ae47d0eba2ba4bf0390091a7c5
andeliero92 <andeliero92@gmail.com>
2015-11-11 11:15:10 +0100

    configurazione database CI

--- bin/initialize-ci.sh
+++ bin/initialize-ci.sh
@@ -1,41 +1,37 @@
 #!/bin/sh
 
-#PARENT_DIR=$(dirname $(cd "$(dirname "$0")"; pwd))
-#CI_DIR="$PARENT_DIR/ci-stuff/environment"
+PARENT_DIR=$(dirname $(cd "$(dirname "$0")"; pwd))
+CI_DIR="$PARENT_DIR/ci-stuff/environment"
 
-#ODB_VERSION=${1:-"1.0rc6"}
-#ODB_PACKAGE="orientdb-${ODB_VERSION}"
-#ODB_DIR="${CI_DIR}/${ODB_PACKAGE}"
-#ODB_LAUNCHER="${ODB_DIR}/bin/server.sh"
+ODB_VERSION=${1:-"1.7.9"}
+ODB_DIR="${CI_DIR}/orientdb-community-${ODB_VERSION}"
+ODB_LAUNCHER="${ODB_DIR}/bin/server.sh"
 
 echo "=== Initializing CI environment ==="
 
-#cd "$PARENT_DIR"
+cd "$PARENT_DIR"
 
-#. "$PARENT_DIR/bin/odb-shared.sh"
+. "$PARENT_DIR/bin/odb-shared.sh"
 
-#if [ ! -d "$CI_DIR" ]; then
-  # Fetch the stuff needed to run the CI session.
-#  git clone --quiet git://gist.github.com/1370152.git $CI_DIR
+if [ ! -d "$ODB_DIR" ]; then
+  # Download and extract OrientDB server
+  echo "--- Downloading OrientDB v${ODB_VERSION} ---"
+  odb_download_server $ODB_VERSION $CI_DIR
 
-  # Download and extract OrientDB
-#  echo "--- Downloading OrientDB v${ODB_VERSION} ---"
-#  odb_download "http://orient.googlecode.com/files/${ODB_PACKAGE}.zip" $CI_DIR
-#  unzip -q "${CI_DIR}/${ODB_PACKAGE}.zip" -d $ODB_DIR && chmod +x $ODB_LAUNCHER
-
-  # Copy the configuration file and the demo database
-#  echo "--- Setting up OrientDB ---"
-#  tar xf $CI_DIR/databases.tar.gz -C "${ODB_DIR}/"
-#  cp $PARENT_DIR/ci-stuff/orientdb-server-config.xml "${ODB_DIR}/config/"
-#  cp $PARENT_DIR/ci-stuff/orientdb-server-log.properties "${ODB_DIR}/config/"
-#else
-#  echo "!!! Directory $CI_DIR exists, skipping downloads !!!"
-#fi
+  # Ensure that launcher script is executable and copy configurations file
+  echo "--- Setting up OrientDB ---"
+  chmod +x $ODB_LAUNCHER
+  chmod -R +rw "${ODB_DIR}/config/"
+  cp $PARENT_DIR/ci-stuff/orientdb-server-config.xml "${ODB_DIR}/config/"
+  cp $PARENT_DIR/ci-stuff/orientdb-server-log.properties "${ODB_DIR}/config/"
+else
+  echo "!!! Found OrientDB v${ODB_VERSION} in ${ODB_DIR} !!!"
+fi
 
 # Start OrientDB in background.
-#echo "--- Starting an instance of OrientDB ---"
-#sh -c $ODB_LAUNCHER </dev/null &>/dev/null &
+echo "--- Starting an instance of OrientDB ---"
+sh -c $ODB_LAUNCHER </dev/null &>/dev/null &
 
 # Wait a bit for OrientDB to finish the initialization phase.
-#sleep 5
-#printf "\n=== The CI environment has been initialized ===\n"
+sleep 10
+printf "\n=== The CI environment has been initialized ===\n"
