e5faf9bc2c95e39b638d519435fcd90de8a78397
bsuieric <bogdansuierica@gmail.com>
2015-11-13 15:30:35 +0100

    in object

--- app/algorithm/AlgorithmService.scala
+++ app/algorithm/AlgorithmService.scala
@@ -3,10 +3,10 @@ package algorithm
 /**
   * Rappresents the services of the algorithm
   */
-class AlgorithmService {
+object AlgorithmService {
 
   /**
-    * Method that returns true if the algorithm is ready to make the reccomendation
+    * Method that returns true if the algorithm is ready to make the recommendation
     * @return Boolean
     */
   def ready() : Boolean = ???
