03abbe744b619654f812b9a0f86a40f05ef3973e
bsuieric <bogdansuierica@gmail.com>
2015-11-16 15:33:05 +0100

    occurencesForEachTarget

--- app/algorithm/Id3.scala
+++ app/algorithm/Id3.scala
@@ -1,15 +1,51 @@
 package algorithm
 
+import models.commons.Item
+import models.commons.Behavior
+import scala.collection.mutable.ListBuffer
+
 
 /**
-  *
+  * Trait used to define signature of the supported methods by Id3 algorithm
   */
 trait Id3{
-  def entropy(list: List[Int]): Double
+  def entropy(behaviorList : List[Behavior]): Double
+  //def gain()
 }
 
-
-
+/**
+  * Object that implements the methods of the trait
+  * @extends Id3
+  */
 object Id3Impl extends Id3 {
-  def entropy(list: List[Int]): Double = ???
+
+  /**
+    * Method that counts the occurences for each target
+    * @param itemList
+    * @param behaviorList
+    * @return List[(Item, Int)] returns for every Item the number of occurences
+    */
+  def occurencesForEachTarget(itemList : List[Item], behaviorList: List[Behavior]): List[(Item, Int)] = {
+    var numberTargetOccurences = new ListBuffer[Int]()
+    for (i <- itemList){
+      var sum: Int = 0
+      for (b <- behaviorList){
+        if (i == b.item)
+          sum = sum+1
+      }
+      numberTargetOccurences += sum
+    }
+    var output = (itemList zip numberTargetOccurences.toList)
+    output
+  }
+
+  def entropy(behaviorList : List[Behavior]): Double = ???
+
+  /**
+    *
+    * @param behaviorList
+    * @return
+    */
+
+
 }
