b3e13369cae59de7607a32f11d3c6500c7f6d021
andeliero92 <andeliero92@gmail.com>
2015-11-16 10:04:35 +0100

    stesura controller

--- app/controllers/BridgeController.scala
+++ app/controllers/BridgeController.scala
@@ -1,21 +1,38 @@
 package controllers
 
-import algorithm.AlgorithmService
+import algorithm.{Id3Service, AlgorithmService}
+import models.commons._
 import play.api._
-import play.api.libs.json._
 import play.api.mvc._
+import play.api.libs.json._
+
 
 /** An instance that receives recommendation calls from client
   *
   */
-class BridgeController extends Controller{
+class BridgeController extends Controller {
   this: Controller =>
 
-  var algorithmService: AlgorithmService = new AlgorithmService()
+  var algorithmService: AlgorithmService = Id3Service
 
-  def ready = Action {
+  def ready() = Action {
     val ready: Boolean = algorithmService.ready()
     Ok(Json.obj("ready" -> ready))
+  }
+
+  def insertBehavior() = Action { request =>
+    val jsonObject = request.body.asJson
+
+
+
+    /*val behavior: Behavior = Behavior(
+      Item(Tag("item:uno") :: Tag("item:due") :: Tag("item:tre") :: Nil ),
+      Interaction( WidgetTag("wtag:sport"), "clicked") ::
+        Interaction(WidgetTag("wtag::mountain"), "onhover") :: Nil
+    )
+    val response = Json.toJson(behavior)*/
+
+    Ok
   }
 
 }
